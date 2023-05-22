b65b2998e64116aeef513ef4556b27cc8424eba7
xun99 <huxun200099@gmail.com>
2023-05-22 15:03:00 +0100

    assignment 1

--- assignment1/send.py
+++ assignment1/send.py
@@ -10,13 +10,13 @@ def randomword(length):
 
 def send_random_traffic(num_packets, interface, src_ip, dst_ip):
     dst_mac = "00:00:00:00:00:01"
-    src_mac= "00:00:00:00:00:02"
+    src_mac= "0c:37:96:5f:89:ec"
     total_pkts = 0
     port = 1024
     for i in range(num_packets):
-            data = randomword(22)
+            data = randomword(458)
             p = Ether(dst=dst_mac,src=src_mac)/IP(dst=dst_ip,src=src_ip)
-            p = p/UDP(sport= 50000, dport=port)/Raw(load=data)
+            p = p/TCP(sport= 5555, dport=port)/Raw(load=data)
             sendp(p, iface = interface, inter = 0.01)
             # If you want to see the contents of the packet, uncomment the line below
             # print(p.show())
