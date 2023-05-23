3d1cb121c0e5e0a56ec3db5a83231345ab6a6bbd
xun99 <huxun200099@gmail.com>
2023-05-23 12:45:48 +0100

    assignment 2 completed

--- assignment2/plot.py
+++ assignment2/plot.py
@@ -3,17 +3,17 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 # parameters to modify
-filename="test.data"
-label='label'
-xlabel = 'xlabel'
-ylabel = 'ylabel'
-title='Simple plot'
-fig_name='test.png'
+filename="processed_iperf3_1.txt"
+label='TCP'
+xlabel = 'time/sec'
+ylabel = 'measured bandwidth/Mbits/sec'
+title='TCP: Bandwidth'
+fig_name='iperf3_1.png'
 
 
 t = np.loadtxt(filename, delimiter=" ", dtype="float")
-
-plt.plot(t[:,0], t[:,1], label=label)  # Plot some data on the (implicit) axes.
+plt.plot(t, label=label)
+#plt.plot(t[:,0], t[:,1], label=label)  # Plot some data on the (implicit) axes.
 plt.xlabel(xlabel)
 plt.ylabel(ylabel)
 plt.title(title)
